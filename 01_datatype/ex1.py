c8d1ceb9e78b325c0e252fead5f31e286c7fb313
Lufftwaffle <leeseungi4105@dimigo.hs.kr>
2026-08-20 12:47:39 +0900

    datatype

---
+++ 01_datatype/ex1.py
@@ -0,0 +1,33 @@
+# # 변수
+# a = 2
+# b = 3
+# print(a, end="")
+# print(b)
+# print(a, b, sep=",")
+
+# a = (2, b) = 3 # => a = 2, b = 3 튜플 언패킹
+# # 대신 a = 2; b = 3
+# a, b = 2, 3
+# print(a, b)
+
+# a = b = c = 0
+
+# 값 swap
+a, b = 2, 3
+a, b = b, a
+print(a, b)
+
+
+# 변수명 규칙(C와 동일)
+# 알파벳, 숫자, 특수문자(_)만 가능
+# 숫자로 시작 X
+# 대소문자 구분
+# 예약어 사용 X
+
+# snake_case
+# camelCase
+# name! = "kor"
+# 2name = "jap"
+# class = "eng"
+이름 = "문화"
+print(이름)
