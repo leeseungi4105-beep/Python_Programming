09cfca39478dfca10262a1ff470118dbd6e48f19
Lufftwaffle <leeseungi4105@dimigo.hs.kr>
2026-08-27 12:44:39 +0900

    8/27

--- 01_datatype/ex4.py
+++ 01_datatype/ex4.py
@@ -28,4 +28,20 @@ print("Hello" * 10)
 # print("Hello" + 10)  X
 print("Hello" + str(10))
 print(int("10") + int("2"))
-print(eval("10" + "2"))
+print(eval("10" + "2"))  # 되나
+
+name = "pororo"
+age = 23
+print(f"이름: {name}, 나이: {age}세")
+print(f"내년 나이: {age + 1}세")
+print(f"{name.upper()}")
+
+pi = 3.1415926535897932384626433832795028841971693
+print(f"{pi:.3f}")
+print(f"{pi:.0f}")
+num = 123456789
+print(f"{num:,}")
+print(f"{num:15d}")
+print(f"{num:<15d}")
+print(f"{num:015d}")
+print(f"{num:015,d}")
